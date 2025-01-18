610fcec4d4ecfa6851589b79d5b25d3288d48c7f
Rohitkashyap-22 <158748049+Rohitkashyap-22@users.noreply.github.com>
2025-01-18 08:01:17 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs not browser so its invalid here
+
+console.log(3
+    +3)           //code readability should be high//
+
+console.log("ROHIT")
+
+//datatypes : number, bigint, string (in double qoutes""), boolean,
+//null=>standalone value
+//undefined=>
+//symbol--in react for unique
+
+//OBJECT--
+
+console.log(typeof "ROHIT");    //string
+console.log(typeof null);       //object**
+console.log(typeof undefined);  //undefined*          
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,13 @@
 console.log("ROHIT")
+
+const accId=2211
+let myName="ROHIT"
+var pass="123"
+
+
+//const can not be modified
+//var never used
+myName="Rohit Kashyap"
+pass="000"
+
+console.table([accId, myName, pass])
